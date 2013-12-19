2ee9b5f9ba36f6ff9008f76f2500a03cd24c1b60
Jean-Philippe Chateau <contact@jpchateau.com>
2013-12-19 11:31:10 +0100

    init bundle

---
+++ Service/Links.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ *
+ * @author:  Gabriel BONDAZ <gabriel.bondaz@idci-consulting.fr>
+ * @author:  Jean-Philippe CHATEAU <jp.chateau@trepia.fr>
+ * @license: GPL
+ *
+ */
+
+namespace Tms\Bundle\RestBundle\Service;
+
+class Links
+{
+
+}
