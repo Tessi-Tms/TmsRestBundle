d3b5b57e6cea7eba17b9cef4f8e3694c7ff5cc9e
Jean-Philippe Chateau <contact@jpchateau.com>
2013-12-27 15:22:11 +0100

    linkRequestListener

---
+++ DependencyInjection/Compiler/RestLinkCompilerPass.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+ *
+ * @author:  Jean-Philippe CHATEAU <jp.chateau@trepia.fr>
+ * @license: GPL
+ *
+ */
+
+namespace Tms\Bundle\RestBundle\DependencyInjection\Compiler;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\DependencyInjection\Reference;
+use Symfony\Component\DependencyInjection\DefinitionDecorator;
+
+class RestLinkCompilerPass implements CompilerPassInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function process(ContainerBuilder $container)
+    {
+        if (!$container->hasDefinition('kernel.listener.link_request_listener')) {
+            return;
+        }
+
+        $definition = $container->getDefinition('kernel.listener.link_request_listener');
+        $taggedServices = $container->findTaggedServiceIds('rest.link');
+        foreach ($taggedServices as $id => $attributes) {
+            if (empty($attributes[0]['class'])) {
+                continue;
+            }
+            $definition->addMethodCall(
+                'addManager',
+                array($attributes[0]['class'], new Reference($id))
+            );
+        }
+    }
+}
--- TmsRestBundle.php
+++ TmsRestBundle.php
@@ -10,11 +10,14 @@ namespace Tms\Bundle\RestBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Tms\Bundle\RestBundle\DependencyInjection\Compiler\RestLinkCompilerPass;
 
 class TmsRestBundle extends Bundle
 {
     public function build(ContainerBuilder $container)
     {
         parent::build($container);
+
+        $container->addCompilerPass(new RestLinkCompilerPass());
     }
 }
