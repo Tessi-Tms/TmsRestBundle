f3810dff438a017e25b67743560c0887f6f551a3
Gnucki <tprelot@gmail.com>
2014-07-15 19:01:32 +0200

    Retrieve actions automaticaly

--- Formatter/AbstractHypermediaFormatter.php
+++ Formatter/AbstractHypermediaFormatter.php
@@ -10,9 +10,11 @@
 
 namespace Tms\Bundle\RestBundle\Formatter;
 
-use Tms\Bundle\RestBundle\Criteria\CriteriaBuilder;
 use Symfony\Component\Routing\Router;
 use JMS\Serializer\Serializer;
+use FOS\RestBundle\Routing\Loader\Reader\RestControllerReader;
+use Tms\Bundle\RestBundle\Criteria\CriteriaBuilder;
+use Tms\Bundle\RestBundle\Request\ParamReaderProvider;
 
 abstract class AbstractHypermediaFormatter
 {
@@ -24,6 +26,8 @@ abstract class AbstractHypermediaFormatter
     protected $serializer;
     protected $router;
     protected $criteriaBuilder;
+    protected $controllerReader;
+    protected $paramReaderProvider;
 
     // Formatters default attributes
     protected $currentRouteName;
@@ -31,16 +35,27 @@ abstract class AbstractHypermediaFormatter
 
     // Actions
     protected $actions = array();
+    protected $controllers = array();
 
     /**
      * Constructor
      */
-    public function __construct(Router $router, CriteriaBuilder $criteriaBuilder, Serializer $serializer, $currentRouteName, $format)
+    public function __construct(
+        Router $router,
+        CriteriaBuilder $criteriaBuilder,
+        Serializer $serializer,
+        RestControllerReader $controllerReader,
+        ParamReaderProvider $paramReaderProvider,
+        $currentRouteName,
+        $format
+    )
     {
         // Services
         $this->router = $router;
         $this->criteriaBuilder = $criteriaBuilder;
         $this->serializer = $serializer;
+        $this->controllerReader = $controllerReader;
+        $this->paramReaderProvider = $paramReaderProvider;
 
         // Formatters default attributes
         $this->currentRouteName = $currentRouteName;
@@ -103,7 +118,7 @@ abstract class AbstractHypermediaFormatter
      * 
      * @return AbstractHypermediaFormatter This.
      */
-    public function setAction(
+    public function addAction(
         $name,
         $method,
         $url,
@@ -111,7 +126,8 @@ abstract class AbstractHypermediaFormatter
         array $optionalParams = array()
     )
     {
-        $this->actions[$name] = array(
+        $this->actions[] = array(
+            'rel' => $name,
             'href' => $url,
             'method' => $method,
             'requiredParams' => $requiredParams,
@@ -128,19 +144,85 @@ abstract class AbstractHypermediaFormatter
      */
     protected function formatActions()
     {
+        $actions = $this->formatControllersActions();
+var_dump($actions);die;
+        return array_merge($this->actions, $actions);
+    }
+
+    /**
+     * Add a controller containing related actions
+     *
+     * @param object $controller A controller class.
+     *
+     * @return AbstractHypermediaFormatter This.
+     */
+    public function addActionsController($controller)
+    {
+        $this->controllers[] = $controller;
+
+        return $this;
+    }
+
+    /**
+     * Format the actions given by the controllers
+     *
+     * @return array
+     */
+    protected function formatControllersActions()
+    {
         $actions = array();
 
-        foreach ($this->actions as $name => $action) {
-            $actions[$name] = array(
-                'rel' => $name,
-                'href' => $action['href'],
-                'method' => $action['method'],
-                'requiredParams' => $action['requiredParams'],
-                'optionalParams' => $action['optionalParams']
-            );
-        }
+        foreach ($this->controllers as $controller) {
+            $paramReader = $this->paramReaderProvider->provide();
+            $class = new \ReflectionClass($controller);
 
-        return $actions;
+            foreach ($class->getMethods() as $method) {
+                $methodName = $method->getName();
+                $actionMethodSuffix = 'Action';
+
+                $routeCollection = $this->controllerReader->read($class);
+
+                if ($actionMethodSuffix === substr($methodName, 0 - strlen($actionMethodSuffix))) {
+                    foreach ($routeCollection->all() as $routeName => $route) {
+                        if ($methodName === $route->getDefault('_controller')) {
+                            $actionName = $routeName;
+                        }
+                    }
+
+                    if (isset($actionName)) {
+                        $route = $routeCollection->get($actionName);
+                        $httpMethods = $route->getMethods();
+                        $url = sprintf('http://%s%s',
+                            $route->getHost(),
+                            $route->getPath()
+                        );
+
+                        $requiredParams = array();
+                        $optionalParams = array();
+
+                        foreach ($paramReader->getParamsFromMethod($method) as $name => $param) {
+                            if ($param->nullable) {
+                                $optionalParams[$name] = $param->requirements;
+                            } else {
+                                $requiredParams[$name] = $param->requirements;
+                            }
+                        }
+
+                        foreach ($httpMethods as $httpMethod) {
+                            $actions[] = array(
+                                'rel' => $actionName,
+                                'href' => $url,
+                                'method' => $httpMethod,
+                                'requiredParams' => $requiredParams,
+                                'optionalParams' => $optionalParams
+                            );
+                        }
+                    }
+                }
+            }
+
+            return $actions;
+        }
     }
 
     /**
