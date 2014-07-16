3e2aa1cc1b35f6f421b6da40029fce4ba4fb0101
Gnucki <tprelot@gmail.com>
2014-07-16 17:57:30 +0200

    Retrieve actions automaticaly from a controller name

---
+++ Request/RequestProvider.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Tms\Bundle\RestBundle\Request;
+
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * RequestProvider is a basic implementation of a 
+ * provider of request.
+ *
+ * @author Thomas Prelot <thomas.prelot@tessi.fr>
+ */
+class RequestProvider implements RequestProviderInterface
+{
+    /**
+     * The services container.
+     *
+     * @var ContainerInterface
+     */
+    private $container;
+
+    /**
+     * Constructor.
+     *
+     * @param ContainerInterface $container The services container.
+     */
+    public function __construct(ContainerInterface $container)
+    {
+        $this->container = $container;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function provide()
+    {
+        return $this->container->get('request');
+    }
+}
