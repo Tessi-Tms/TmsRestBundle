cc3918e8aaf9954df86b8dc90f2bfd604b633428
Benjamin TARDY <pandix211@gmail.com>
2014-10-10 14:30:32 +0200

    Add an array provider/formatter

---
+++ Formatter/ArrayHypermediaFormatter.php
@@ -0,0 +1,119 @@
+<?php
+
+namespace Tms\Bundle\RestBundle\Formatter;
+
+use Symfony\Component\Routing\Router;
+use Symfony\Component\Routing\Route;
+use Symfony\Component\Config\Loader\LoaderInterface;
+use JMS\Serializer\Serializer;
+use Tms\Bundle\RestBundle\Request\ParamReaderProviderInterface;
+use Tms\Bundle\RestBundle\Request\RequestProviderInterface;
+
+class ArrayHypermediaFormatter extends AbstractHypermediaFormatter
+{
+    /**
+     * Array of data
+     *
+     * @var array
+     */
+    protected $data;
+
+
+    /**
+     * Constructor
+     *
+     * @param Router                       $router              Instance of Router
+     * @param Serializer                   $serializer          Instance of Serializer
+     * @param LoaderInterface              $routeLoader         Instance of LoaderInterface
+     * @param ParamReaderProviderInterface $paramReaderProvider Instance of ParamReaderProviderInterface
+     * @param RequestProviderInterface     $requestProvider     Instance of RequestProviderInterface
+     * @param string                       $currentRouteName    The current route name
+     * @param string                       $format              The output format
+     * @param array                        $data                The data
+     */
+    public function __construct(
+        Router $router,
+        Serializer $serializer,
+        LoaderInterface $routeLoader,
+        ParamReaderProviderInterface $paramReaderProvider,
+        RequestProviderInterface $requestProvider,
+        $currentRouteName,
+        $format,
+        array $data = array()
+    )
+    {
+        $this->setData($data);
+
+        parent::__construct(
+            $router,
+            $serializer,
+            $routeLoader,
+            $paramReaderProvider,
+            $requestProvider,
+            $currentRouteName,
+            $format
+        );
+    }
+
+    /**
+     * Set the array of data
+     *
+     * @param array $array An array of data
+     * @return ArrayHypermediaFormatter
+     */
+    public function setData(array $data = array())
+    {
+        $this->data = $data;
+
+        return $this;
+    }
+
+    /**
+     * Format raw data to have hypermedia metadata in output
+     *
+     * @return array
+     */
+    protected function formatMetadata()
+    {
+        return array(
+            'type' => $this->getType(),
+            AbstractHypermediaFormatter::SERIALIZER_CONTEXT_GROUP_NAME => $this->getSerializerContextGroup()
+        );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function formatData()
+    {
+        return is_array($this->data) ? $this->data : array();
+    }
+
+    /**
+     * Format raw data to have hypermedia links in output
+     *
+     * @return array
+     */
+    protected function formatLinks()
+    {
+        return null;
+    }
+
+    /**
+     * Give object type
+     *
+     * @return string
+     */
+    protected function getType()
+    {
+        return 'array';
+    }
+
+    /**
+     * {@inheritdoc }
+     */
+    protected function getSerializerContextGroup()
+    {
+        return AbstractHypermediaFormatter::SERIALIZER_CONTEXT_GROUP_COLLECTION;
+    }
+}
