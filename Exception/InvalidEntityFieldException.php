b620140a93ef4775ea6ebd47f82998822995d91e
Gnucki <tprelot@gmail.com>
2015-10-21 18:13:21 +0200

    Add status code parameter

--- Exception/InvalidEntityFieldException.php
+++ Exception/InvalidEntityFieldException.php
@@ -2,6 +2,8 @@
 
 namespace Tms\Bundle\RestBundle\Exception;
 
+use FOS\RestBundle\Util\Codes;
+
 /**
  * Exception thrown when an entity field is invalid.
  *
@@ -10,12 +12,33 @@ namespace Tms\Bundle\RestBundle\Exception;
 class InvalidEntityFieldException extends \InvalidArgumentException
 {
     /**
+     * The HTTP status code
+     *
+     * @var integer
+     */
+    private $statusCode = Codes::HTTP_BAD_REQUEST;
+
+    /**
      * Constructor
      *
      * @param array $errorList
      */
-    public function __construct($errorList)
+    public function __construct($errorList, $statusCode = null)
     {
         parent::__construct(print_r($errorList, true), 0, null);
+
+        if ($statusCode) {
+            $this->statusCode = $statusCode;
+        }
+    }
+
+    /**
+     * Constructor
+     *
+     * @param array $errorList
+     */
+    public function getStatusCode()
+    {
+        return $this->statusCode;
     }
 }
